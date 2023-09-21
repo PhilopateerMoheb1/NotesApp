363e3468fbbe692f48e304bf521aaa0d1077d54f
Philopateer Moheb <philopateermoheb1901202@gmail.com>
2023-09-21 16:46:11 +0300

    Feat:generate type adapter of NotesModel

--- lib/Models/NotesModel.dart
+++ lib/Models/NotesModel.dart
@@ -1,9 +1,16 @@
-class NotesModel {
+import 'package:hive/hive.dart';
+part 'NotesModel.g.dart';
+
+@HiveType(typeId: 0)
+class NotesModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subTitle;
+  @HiveField(2)
   final String date;
 
-  const NotesModel({
+  NotesModel({
     required this.title,
     required this.subTitle,
     required this.date,
