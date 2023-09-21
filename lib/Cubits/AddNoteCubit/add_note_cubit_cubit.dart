2c166fc48b9b1453c3339f6f86f28770f346638c
Philopateer Moheb <philopateermoheb1901202@gmail.com>
2023-09-21 19:14:25 +0300

    Feat:AddNote in ADDnotecubit

--- lib/Cubits/AddNoteCubit/add_note_cubit_cubit.dart
+++ lib/Cubits/AddNoteCubit/add_note_cubit_cubit.dart
@@ -1,8 +1,23 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:meta/meta.dart';
+import 'package:notesapp/Models/NotesModel.dart';
+import 'package:notesapp/constants.dart';
 
 part 'add_note_cubit_state.dart';
 
 class AddNoteCubitCubit extends Cubit<AddNoteCubitState> {
   AddNoteCubitCubit() : super(AddNoteCubitInitial());
+  addNote(NotesModel notesModel) async {
+    emit(AddNoteCubitLoading());
+    try {
+      var noteBox = Hive.box<NotesModel>(knotesBox);
+      await noteBox.add(notesModel);
+      emit(AddNoteCubitSuccess());
+    } on Exception catch (e) {
+      emit(
+        AddNoteCubitFailure(msg: e.toString()),
+      );
+    }
+  }
 }
