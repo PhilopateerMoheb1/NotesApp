aac1387dfed8eee4a9cc03ba249a07b649ff3b14
Philopateer Moheb <philopateermoheb1901202@gmail.com>
2023-09-21 13:20:32 +0300

    Feat:NotesModel

---
+++ lib/Models/NotesModel.dart
@@ -0,0 +1,11 @@
+class NotesModel {
+  final String title;
+  final String subTitle;
+  final String date;
+
+  const NotesModel({
+    required this.title,
+    required this.subTitle,
+    required this.date,
+  });
+}
