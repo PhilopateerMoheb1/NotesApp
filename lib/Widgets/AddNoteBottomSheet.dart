8165439f315931aa4be3f0a7cd607a0dfafde4d8
Philopateer Moheb <philopateermoheb1901202@gmail.com>
2023-09-22 10:53:07 +0300

    Feat:Bloc consumer in AddNoteBottom Sheet

--- lib/Widgets/AddNoteBottomSheet.dart
+++ lib/Widgets/AddNoteBottomSheet.dart
@@ -1,22 +1,33 @@
 import 'dart:ffi';
 
 import 'package:flutter/material.dart';
-import 'package:notesapp/Widgets/CustomWideButton.dart';
-
-import '../constants.dart';
-import 'CustomTextFieldBottom.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:notesapp/Cubits/AddNoteCubit/add_note_cubit_cubit.dart';
 import 'TextFieldFormWidget.dart';
 
 class AddNoteBottomSheetWidget extends StatelessWidget {
   const AddNoteBottomSheetWidget({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const SingleChildScrollView(
-      child: TextFieldFormWidget(
-        firstTextFieldHintText: "Title",
-        secondTextFieldHintText: "Content",
-        textButton: "Add",
+    return SingleChildScrollView(
+      child: BlocConsumer<AddNoteCubitCubit, AddNoteCubitState>(
+        listener: (context, state) {
+          if (state is AddNoteCubitFailure) {
+          } else if (state is AddNoteCubitSuccess) {
+            Navigator.pop(context);
+          }
+        },
+        builder: (context, state) {
+          return ModalProgressHUD(
+            inAsyncCall: state is AddNoteCubitLoading ? true : false,
+            child: const TextFieldFormWidget(
+              firstTextFieldHintText: "Title",
+              secondTextFieldHintText: "Content",
+              textButton: "Add",
+            ),
+          );
+        },
       ),
     );
   }
