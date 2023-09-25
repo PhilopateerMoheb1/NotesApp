46730b04e2afd0239fbc684a346dfedf7d73cc24
Philopateer Moheb <philopateermoheb1901202@gmail.com>
2023-09-25 18:07:59 +0300

    Feat:Bloc Povider in notes page & bloc builder in NotesListview

--- lib/Screens/NotesPage.dart
+++ lib/Screens/NotesPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notesapp/Cubits/GetNoteCubit/get_note_cubit.dart';
 import 'package:notesapp/Widgets/AddNoteBottomSheet.dart';
 
 import '../Widgets/CustomIcon.dart';
@@ -10,35 +12,38 @@ class NotesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-              isScrollControlled: true,
-              context: context,
-              builder: (context) {
-                return const AddNoteBottomSheetWidget();
-              });
-        },
-        child: const Icon(Icons.add),
-      ),
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        title: const Text("Notes"),
-        elevation: 0,
-        actions: const [
-          Padding(
-            padding: EdgeInsets.only(right: 20.0),
-            child: CustomIcon(
-              icon: Icon(
-                Icons.search,
-                size: 28,
+    return BlocProvider(
+      create: (context) => GetNoteCubit(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+                isScrollControlled: true,
+                context: context,
+                builder: (context) {
+                  return const AddNoteBottomSheetWidget();
+                });
+          },
+          child: const Icon(Icons.add),
+        ),
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          title: const Text("Notes"),
+          elevation: 0,
+          actions: const [
+            Padding(
+              padding: EdgeInsets.only(right: 20.0),
+              child: CustomIcon(
+                icon: Icon(
+                  Icons.search,
+                  size: 28,
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
+        body: const NotesBodyView(),
       ),
-      body: const NotesBodyView(),
     );
   }
 }
--- lib/Widgets/NotesListView.dart
+++ lib/Widgets/NotesListView.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notesapp/Cubits/GetNoteCubit/get_note_cubit.dart';
+import 'package:notesapp/Screens/EditPageView.dart';
 import 'package:notesapp/Widgets/NotesViewWidgetItem.dart';
 
 class NotesListView extends StatelessWidget {
@@ -14,23 +17,33 @@ class NotesListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 10.0),
-        child: ListView.builder(
-          padding: EdgeInsets.zero,
-          itemBuilder: (context, index) {
-            if (i == 4) {
-              i = 0;
-            }
-            return NotesViewWidgetItem(
-              title: "Philo",
-              subtitle: "Philo",
-              color: colors[i++],
-            );
-          },
-        ),
-      ),
+    return BlocBuilder<GetNoteCubit, GetNoteState>(
+      builder: (context, state) {
+        if (state is GetNoteSuccess) {
+          return Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10.0),
+              child: ListView.builder(
+                padding: EdgeInsets.zero,
+                itemBuilder: (context, index) {
+                  if (i == 4) {
+                    i = 0;
+                  }
+                  return NotesViewWidgetItem(
+                    title: "Philo",
+                    subtitle: "Philo",
+                    color: colors[i++],
+                  );
+                },
+              ),
+            ),
+          );
+        } else if (state is GetNoteFailure) {
+          return Text(state.errMsg);
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
     );
   }
 }
